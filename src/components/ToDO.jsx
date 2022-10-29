1449a6bccc148fd762636d33afc83490b8998d7d
MSameer192 <sameer192.official@gmail.com>
2022-10-30 01:53:20 +0500

    ALL FUNCTIONALITY DONE

--- src/components/ToDO.jsx
+++ src/components/ToDO.jsx
@@ -4,25 +4,43 @@ export default function ToDO() {
 
   const [toDoList, setToDoList] = useState('');
   const [list, setList] = useState([]);
+  const [editCheck, setEditCheck] = useState(false);
+  const [editValue, setEditValue] = useState("");
 
-  const OnChange = (e) => {
-    setToDoList(e.target.value);
-      
-  };
+  // const addFunc = () => {
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-  };
+  //   if(toDoList)
+  //     {
+  //     let newList = [...list, toDoList];
+  //     setList(newList);
+  //     setToDoList("");
+  //   } else {alert("ALERT! TASK BOX IS EMPTY, PLEASE PUT YOUR TASK")}
+  // };
 
-  const myFuncText = () => {
-    let newList = [...list, toDoList];
-    setList(newList);
-    setToDoList("");
-    console.log(newList);
+  const addFunc = () => {
+
+    if (editCheck) {
+      list[editValue] = toDoList
+      setList(list)
+
+    } else {
+      if (toDoList) {
+        setList([...list, toDoList])
+        } else { alert("ALERT! TASK BOX IS EMPTY, PLEASE PUT YOUR TASK") }
+      }
+      setToDoList("");
   };
 
   
-  
+
+  function editFunc(index) {    
+    setToDoList(list[index])
+    setEditValue([index])
+    setEditCheck(true)
+  }
+
+
+
   const deleteAll = () => {
     let check = window.confirm('You want to delete all list')
     if (check) {
@@ -36,14 +54,15 @@ export default function ToDO() {
     setList(newList)
   }
 
-  // const editList = (index) => {
-  //     list.map(item => {
-  //       if(item.index === index)
-  //         return {...list, title: toDoList }
-  //       else return list;
-  //     })
-  
-  // }
+  const OnChange = (e) => {
+    setToDoList(e.target.value);
+
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
 
 
 
@@ -52,18 +71,17 @@ export default function ToDO() {
       <h1 className='text-light'>Todo list</h1>
 
       <form className="my-5 form" onSubmit={onSubmit}>
-       
+
         <div className="d-flex my-row">
-          
-          
-            
-          {<input type="text" value={toDoList} minLength={1} required onChange={OnChange} className='py-2 ps-3 my-input' placeholder='Enter your task' />}
-          {list === '' ? alert("ALERT! PLEASE ADD YOUR TASK"): ''}
-          <button type="submit" className="btn btn-primary my-button ms-2" 
-            onClick={myFuncText}>ADD
+
+
+
+          {<input type="text" value={toDoList} onChange={OnChange} className='py-2 ps-3 my-input' placeholder='Enter your task' />}
+          <button type="submit" className="btn btn-primary my-button ms-2"
+            onClick={addFunc}>ADD
           </button>
 
-        </div>    
+        </div>
 
 
         <div className="text-light">
@@ -72,22 +90,27 @@ export default function ToDO() {
           {list.length > 0 ? (
 
             <div className='mt-5'>
-              {list.map((item, index) => {
+              {
+                  list.map((item, index) => {
 
-                const deleteList = () => {
-                  onDelete(index)
-                };
+                    const deleteList = () => {
+                      onDelete(index)
+                    };
 
-                return (
-                  <div className="box">
+                    return (
+                      <div className="box">
 
-                    <div className='myBox'>
-                        <strong>{item}</strong>
-                    </div>
-                    <button type='button' className='btn btn-primary' style={{ width: '100px' }} onClick={deleteList}>DELETE</button>
-                  </div>
+                        <div className='myBox'>
+                          <strong key={index}>{item}</strong>
+                        </div>
+                        <button type='button' className='btn btn-primary me-3' style={{ width: '100px' }} onClick={() => { deleteList(index) }} >DELETE</button>
+
+                        <button type='button' className='btn btn-primary' style={{ width: '100px' }} onClick={() => { editFunc(index) }}>EDIT</button>
+                      </div>
+                    )
+                  }
                 )
-              })}
+              }
             </div>
           )
             : (
@@ -97,11 +120,11 @@ export default function ToDO() {
 
         </div>
 
-            {list.length > 0 ? (
-              <div>
-                <button type='button' className='btn btn-primary mt-5' style={{ width: '150px' }} onClick={deleteAll}>DELETE ALL LIST</button>
-              </div>
-            ) : (null)}
+        {list.length > 0 ? (
+          <div>
+            <button type='button' className='btn btn-primary mt-5' style={{ width: '150px' }} onClick={deleteAll}>DELETE ALL LIST</button>
+          </div>
+        ) : (null)}
       </form>
     </div>
   );
