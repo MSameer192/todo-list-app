f103461ce743d12df95ac032e65eb7c474d48750
MSameer192 <sameer192.official@gmail.com>
2022-10-29 15:01:29 +0500

    UPDATE

--- src/components/ToDO.jsx
+++ src/components/ToDO.jsx
@@ -7,6 +7,8 @@ export default function ToDO() {
 
   const OnChange = (e) => {
     setToDoList(e.target.value);
+  
+    
   };
 
   const onSubmit = (e) => {
@@ -45,28 +47,30 @@ export default function ToDO() {
   // }
 
 
-  
 
   return (
     <div className='container text-center mt-5 pt-5' >
       <h1 className='text-light'>Todo list</h1>
 
-      <form className="row my-5 mx-auto" style={{ width: '400px' }} onSubmit={onSubmit}>
-      
-        <input type="text" value={toDoList} minLength={1} required onChange={OnChange} className='col-lg-6' placeholder='Enter your task' style={{ width: '290px' }} />
+      <form className="my-5 form" onSubmit={onSubmit}>
+       
+        <div className="d-flex my-row">
+          
+          <input type="text" value={toDoList} minLength={1} onChange={OnChange} className='py-2 ps-3 my-input' placeholder='Enter your task' />
 
-        <button type="submit" className="btn btn-primary col-lg-6 ms-2" style={{ width: '100px' }}
-          onClick={myFuncText}>ADD
-        </button>
+          <button type="submit" className="btn btn-primary my-button ms-2" 
+            onClick={myFuncText}>ADD
+          </button>
 
-        
+        </div>    
+
 
         <div className="text-light">
 
 
           {list.length > 0 ? (
 
-            <ul className='mt-5'>
+            <div className='mt-5'>
               {list.map((item, index) => {
 
                 const deleteList = () => {
@@ -75,28 +79,29 @@ export default function ToDO() {
 
                 return (
                   <div className="box">
-                    <li><strong>{item}</strong></li>
+
+                    <div className='myBox'>
+                        <strong>{item}</strong>
+                    </div>
                     <button type='button' className='btn btn-primary' style={{ width: '100px' }} onClick={deleteList}>DELETE</button>
                     {/* <button type='button' className='btn btn-primary' style={{ width: '100px' }} onClick={editList}>RE-WRITE</button> */}
                   </div>
                 )
               })}
-            </ul>
+            </div>
           )
             : (
-              <img src="./img.svg" alt="" className='mt-5 img-fluid' />
+              <img src="./img.svg" alt="" className='mt-5 img-fluid' style={{ width: '400px' }} />
             )
           }
 
         </div>
-        {list.length > 0 ? (
-          <div>
-            <button type='button' className='btn btn-primary' style={{ width: '100px' }} onClick={deleteAll}>DELETE ALL LIST</button>
-          </div>
-        ) : (null)}
 
-
-
+            {list.length > 0 ? (
+              <div>
+                <button type='button' className='btn btn-primary mt-5' style={{ width: '100px' }} onClick={deleteAll}>DELETE ALL LIST</button>
+              </div>
+            ) : (null)}
       </form>
     </div>
   );
